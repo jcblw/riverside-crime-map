5d5fbbb67cafeaa22c7e37de11ec836cdb6791db
Jacob Lowe <jacoblowe2.0@gmail.com>
2013-06-07 22:40:27 -0700

    setting up clusters

--- app.js
+++ app.js
@@ -4,32 +4,49 @@ express = require("express"),
 http = require("http"),
 request = require("request"),
 path = require("path"),
-app = express();
+cluster = require("cluster"),
+connection = require("mangos");
 
-app.configure(function(){
-	app.set('port', process.env.PORT || 3000);
-	app.use(app.router);
-	app.use(express.bodyParser());
-	app.use(express.cookieParser());
-	app.use(express.static(path.join(__dirname, 'public')));
-})
+if (cluster.isMaster) {
+	// Fork workers.
+	for (var i = 0; i < 2; i++) {
+		cluster.fork();
+	}
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-})
+	cluster.on('exit', function(worker, code, signal) {
+		console.log('worker ' + worker.process.pid + ' died');
+		cluster.fork();
+	});
+} else {
+  // Workers can share any TCP connection
+  // In this case its a HTTP server
+	var
+	app = express(),
+	db = new connection('riverside-crime', '127.0.0.1', 27017),
+	cron = function(job, duration){
+		if( typeof job === "function" ){
+			job(function(){
+				setTimeout(function(){
+					cron(job, duration); // start up process again
+				}, duration)
+			})
+		}
+	},
+	fetch = function(){
+		
+	};
 
-app.get('/', function(req,res){
- res.sendfile(__dirname + '/public/index.html');
-})
-
-app.get("/latest", function(req, res){
-	request("http://www.riversideca.gov/data/dataset/json/27", function(err, response, body){
-		res.json(JSON.parse(body));
+	app.configure(function(){
+		app.set('port', process.env.PORT || 3000);
+		app.use(app.router);
+		app.use(express.bodyParser());
+		app.use(express.cookieParser());
+		app.use(express.static(path.join(__dirname, 'public')));
 	})
-})
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-})
+	http.createServer(app).listen(app.get('port'), function(){
+	  console.log("Server process " + process.pid + " listening on port " + app.get('port'));
+	}) 
+}
 
 
