eed407d13c2773f503958acde6a4c9040577394c
Jacob Lowe <jacoblowe2.0@gmail.com>
2013-06-07 23:22:14 -0700

    setting limit to 100

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ connection = require("mangos");
 
 if (cluster.isMaster) {
 	// Fork workers.
-	for (var i = 0; i < 2; i++) {
+	for (var i = 0; i < 1; i++) {
 		cluster.fork();
 	}
 
@@ -23,6 +23,7 @@ if (cluster.isMaster) {
 	var
 	app = express(),
 	db = new connection('riverside-crime', '127.0.0.1', 27017),
+	//fouroneone = new connection('riverside-crime', '127.0.0.1', 27017),
 	cron = function(job, duration){
 		if( typeof job === "function" ){
 			job(function(){
@@ -32,8 +33,36 @@ if (cluster.isMaster) {
 			})
 		}
 	},
-	fetch = function(){
-		
+	update = function(set, index, length, fn){
+		set.rid = set._id;
+		delete set._id;
+		db.read({rid:set.rid}, function(err, res){
+
+			if(!res.length){
+				console.log("store");
+				db.create(set, function(){});
+			}
+
+			if(length === index + 1){
+				fn();
+				console.log("ready for next queue")
+			}
+
+		})
+	},
+	fetch = function(fn){
+		request("http://www.riversideca.gov/data/dataset/json/27?max=100", function(err, response, body){
+			if(err) fn();
+			else{
+				var data = JSON.parse(body);
+				var length = data.length;
+				console.log(length);
+				for(var i = 0; i < length; i++){
+					var set = data[i];
+					update(set, i, length, fn);
+				}
+			}
+		})
 	};
 
 	app.configure(function(){
@@ -43,6 +72,17 @@ if (cluster.isMaster) {
 		app.use(express.cookieParser());
 		app.use(express.static(path.join(__dirname, 'public')));
 	})
+
+	app.get("/archive", function(req, res){
+		db.read({}, function(err, response){
+			if(err) res.send("Error")
+			else{
+				res.json(response);
+			}
+		})
+	})
+
+	cron(fetch, 1200000);
 
 	http.createServer(app).listen(app.get('port'), function(){
 	  console.log("Server process " + process.pid + " listening on port " + app.get('port'));
